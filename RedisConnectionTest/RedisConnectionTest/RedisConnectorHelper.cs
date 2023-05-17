52630fae242ae64e5304c3cb69a94d063143de0d
LAPTOP-DPS9GSGG\admin <637640@sv.vnua.edu.vn>
2023-05-17 16:00:36 +0700

    Redis cache tutorial

---
+++ RedisConnectionTest/RedisConnectionTest/RedisConnectorHelper.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+
+public class RedisConnectorHelper
+{
+    static RedisConnectorHelper()
+    {
+        RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            return ConnectionMultiplexer.Connect("localhost");
+        });
+    }
+
+    private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+    public static ConnectionMultiplexer Connection
+    {
+        get
+        {
+            return lazyConnection.Value;
+        }
+    }
+}
